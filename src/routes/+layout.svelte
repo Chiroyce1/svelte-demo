e03f99446b08588251e41f911074556517fc82c3
Chiroyce <97374054+Chiroyce1@users.noreply.github.com>
2023-01-07 17:19:58 +0530

    Initial Commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,36 @@
+<script type="ts">
+	// @ts-ignore
+	import NProgress from 'nprogress';
+	import { navigating } from '$app/stores';
+	
+	// NProgress css
+	import 'nprogress/nprogress.css';
+	
+	NProgress.configure({
+		// Full list: https://github.com/rstacruz/nprogress#configuration
+		minimum: 0.2
+	});
+ 
+	$: {
+		if ($navigating) {
+			NProgress.start();
+		}
+		if (!$navigating) {
+			NProgress.done();
+		}
+	}
+</script>
+
+<nav>
+	<a href="/">Home</a>
+	<a href="/test/Test%20Demo">Test</a>
+</nav>
+
+<style>
+	nav > a {
+		color: black;
+		text-decoration: underline;
+	}
+</style>
+
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	let text: String;
+
+	function handleClick() {
+		location.href = `/test/${text}`;
+	}
+</script>
+
+<style>
+	a { color: black; }
+</style>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<input type="text" bind:value={text}>
+<a href={`/test/${text}`}><button>Load</button></a>
